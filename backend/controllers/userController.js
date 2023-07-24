705bc45a7266655c92c465a0ac3790b2208fb835
Yasir Shahid <66242488+yasirshahid@users.noreply.github.com>
2023-07-24 17:09:53 +0500

    add error handler middleware

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,8 +1,12 @@
+import asyncHandler from "express-async-handler";
+
 // @des Auth User/set token
 // @route route POST /ap/users/auth
 // @access public
-const authUser = (req, res) => {
+const authUser = asyncHandler(async (req, res) => {
+  res.status(401);
+  throw new Error("Something went wrong");
   res.status(200).json({ Message: "Auth User" });
-};
+});
 
 export { authUser };
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 
 dotenv.config();
+import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 import userRoutes from "./routes/userRoutes.js";
@@ -11,4 +12,6 @@ const app = express();
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send(`server is ready`));
 
+app.use(notFound);
+app.use(errorHandler);
 app.listen(port, () => console.log(`server listening on ${port}`));
