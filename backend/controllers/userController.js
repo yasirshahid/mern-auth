a9bf95b48f2dabefdccc0a077af0f253abc1690b
Yasir Shahid <66242488+yasirshahid@users.noreply.github.com>
2023-07-25 16:12:52 +0500

    add jwt token logic

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,5 @@
 import asyncHandler from "express-async-handler";
+import generateToken from "../utils/generateToken.js";
 import User from "../models/userModel.js";
 
 // @des Auth User/set token
@@ -21,6 +22,7 @@ const registerUser = asyncHandler(async (req, res) => {
   const user = await User.create({ name, email, password });
 
   if (user) {
+    generateToken(res, user._id);
     res.status(201).json({ _id: user._id, name: user.name, email: user.email });
   } else {
     res.status(400);
