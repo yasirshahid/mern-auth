6805d10f140d4694ddd4cf08f8f3979e31b6422d
Yasir Shahid <66242488+yasirshahid@users.noreply.github.com>
2023-07-25 17:02:28 +0500

    edit code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,19 +7,22 @@ import User from "../models/userModel.js";
 // @access public
 const authUser = asyncHandler(async (req, res) => {
   const { email, password } = req.body;
+
   const user = await User.findOne({ email });
 
   if (user && (await user.matchPassword(password))) {
     generateToken(res, user._id);
-    res.status(201).json({ _id: user._id, name: user.name, email: user.email });
+
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
   } else {
-    res.status(400);
-    throw new Error("Inalid email or password");
+    res.status(401);
+    throw new Error("Invalid email or password");
   }
-
-  res.status(200).json({ Message: "Auth User" });
 });
-
 // @des Register a new user
 // @route route POST /api/users
 // @access public
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -19,7 +19,12 @@ const userSchema = mongoose.Schema(
   },
   { timestamps: true }
 );
+// Match user entered password to hashed password in database
+userSchema.methods.matchPassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword, this.password);
+};
 
+// Encrypt password using bcrypt
 userSchema.pre("save", async function (next) {
   if (!this.isModified("password")) {
     next();
@@ -28,10 +33,6 @@ userSchema.pre("save", async function (next) {
   const salt = await bcrypt.genSalt(10);
   this.password = await bcrypt.hash(this.password, salt);
 });
-
-userSchema.methods.matchPassword = async function (enteredPassword) {
-  return await bcrypt.compare(enteredPassword, this.password);
-};
 
 const User = mongoose.model("User", userSchema);
 export default User;
