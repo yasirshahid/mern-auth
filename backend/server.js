0088047358c8dc64a01b4d07b314a0b549852833
Yasir Shahid <66242488+yasirshahid@users.noreply.github.com>
2023-07-25 13:13:38 +0500

    mongoDB connected

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 import express from "express";
 import dotenv from "dotenv";
-
+import connectdb from "./config/db.js";
 dotenv.config();
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
 import userRoutes from "./routes/userRoutes.js";
-
+connectdb();
 const app = express();
 
 app.use("/api/users", userRoutes);
