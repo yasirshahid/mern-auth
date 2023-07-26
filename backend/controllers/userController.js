608d75de6a5fa30826b824c0d810ff9cfd96b879
Yasir Shahid <66242488+yasirshahid@users.noreply.github.com>
2023-07-26 14:31:38 +0500

    auth middleware and user get profile and update profile code

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -59,13 +59,36 @@ const logoutUser = asyncHandler(async (req, res) => {
 // @route route GET /api/users/profle
 // @access private
 const getUserProfile = asyncHandler(async (req, res) => {
-  res.status(200).json({ Message: "User Profile" });
+  const user = {
+    _id: req.user._id,
+    name: req.user.name,
+    email: req.user.email,
+  };
+  res.status(200).json(user);
 });
 
 // @des Update user profile
 // @route route PUT /api/users/profle
 // @access private
 const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+    const updatedUser = await user.save();
+    res.status(200).json({
+      _id: updatedUser._id,
+      _name: updatedUser.name,
+      _email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error("User Not Found");
+  }
   res.status(200).json({ Message: "Update User Profile" });
 });
 
--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import connectdb from "./config/db.js";
 dotenv.config();
+import cookieParser from "cookie-parser";
 import { notFound, errorHandler } from "./middleware/errorMiddleware.js";
 const port = process.env.PORT || 5000;
 
@@ -10,7 +11,7 @@ connectdb();
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+app.use(cookieParser());
 app.use("/api/users", userRoutes);
 app.get("/", (req, res) => res.send(`server is ready`));
 
