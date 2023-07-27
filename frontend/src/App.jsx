76a6c413cc1f94c6eea68d94fb1542ef0333d2de
Yasir Shahid <66242488+yasirshahid@users.noreply.github.com>
2023-07-27 12:45:43 +0500

    add react router dom

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
-import React from "react";
+import { Outlet } from "react-router-dom";
+import { Container } from "react-bootstrap";
 import Header from "./components/Header";
-import HomeScreen from "./screens/HomeScreen";
 
 const App = () => {
   return (
     <>
       <Header />
-      <HomeScreen />
+      <Container className='my-2'>
+        <Outlet />
+      </Container>
     </>
   );
 };
