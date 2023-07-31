3c504930480cb44ff720ca90357674847d45962c
Yasir Shahid <66242488+yasirshahid@users.noreply.github.com>
2023-07-31 12:56:37 +0500

    Update login Screen

--- frontend/src/screens/LoginScreen.jsx
+++ frontend/src/screens/LoginScreen.jsx
@@ -12,7 +12,39 @@ const LoginScreen = () => {
     console.log("submit");
   };
 
-  return <div>LoginScreen</div>;
+  return (
+    <FormContainer>
+      <h1>Sign In</h1>
+      <Form onSubmit={submitHandler}>
+        <Form.Group className='my-2' controlId='email'>
+          <Form.Label>Email Address</Form.Label>
+          <Form.Control
+            type='email'
+            placeholder='Enter email'
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Form.Group className='my-2' controlId='password'>
+          <Form.Label>password Address</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Enter Password'
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          ></Form.Control>
+        </Form.Group>
+        <Button type='submit' variant='primary' className='mt-3'>
+          Sign In
+        </Button>
+        <Row className='py-3'>
+          <Col>
+            New Customer?<Link to='/register'>Register</Link>
+          </Col>
+        </Row>
+      </Form>
+    </FormContainer>
+  );
 };
 
 export default LoginScreen;
