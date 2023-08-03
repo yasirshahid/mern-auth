3c67e33990ec2fbdf77a049bac773c538b5cd708
Yasir Shahid <66242488+yasirshahid@users.noreply.github.com>
2023-08-03 14:04:36 +0500

    Update Private Route

--- frontend/src/components/PrivateRoute.jsx
+++ frontend/src/components/PrivateRoute.jsx
@@ -1,11 +1,8 @@
 import { Navigate, Outlet } from "react-router-dom";
 import { useSelector } from "react-redux";
 
-import React from "react";
-
-const { userInfo } = useSelector((state) => state.auth);
-
 const PrivateRoute = () => {
+  const { userInfo } = useSelector((state) => state.auth);
   return userInfo ? <Outlet /> : <Navigate to='/login' replace />;
 };
 
